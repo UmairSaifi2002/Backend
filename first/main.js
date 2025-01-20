b4dfe9f2a0f3c4ac498c254441ec6d70557cf711
UmairSaifi <umairsaifi2002@gmail.com>
2025-01-21 00:10:21 +0530

    learn about mymodules

---
+++ first/main.js
@@ -0,0 +1,22 @@
+import {a,b,c,name} from './mymodule.js'
+import obj from './mymodule.js'
+
+// import { createServer } from 'node:http';
+// // agr hame server restart krna h agr koi bhi changes hoga to 
+// // ham nodemon se run krengy file
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1> HELLO WORLD !!</h1>');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+console.log(a,b,c,name);
+
+console.log(obj);
---
+++ first/mymodule.js
@@ -0,0 +1,13 @@
+export const a = 1
+export const b = 2
+export const c = 3
+export const name = 'Umair'
+
+const obj = {
+    a : 'a',
+    b : 'b',
+    c : 'c',
+    name : 'Umair'
+}
+
+export default obj;
